ccf7dee9617843aa0664676ae32969ca82c557ea
pewdiefis <fissgeci1@gmail.com>
2018-04-23 20:19:48 +0200

    Added the todo list first functiongit  add .

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const ejs = require('ejs');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const DB_URI = "mongodb://localhost:27017/todoapp"
+const bodyParser = require('body-parser');
+const router = express.Router();
+const ObjectId = require('mongodb').ObjectID;
 
-
-app.set('view', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'view'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 MongoClient.connect(DB_URI,(err,db) => {
     if (err) {
@@ -17,5 +22,34 @@ MongoClient.connect(DB_URI,(err,db) => {
     todos = db.collection('todos');
     console.log("Login Succsesful " + DB_URI);
 })
+//To put in the database  the values in the  inputs in the index.ejs file
+app.post('/todo/add',(req,res,next) =>{
+    todos.insert({title: req.body.title, description: req.body.description},(err,document) => {
+        if (err) {
+            return
+        }
+        res.redirect('/');
+    });
+});
+//To Display them in  localhost
+app.get('/',(req,res) => {
+    todos.find({}).toArray((err,docs) => {
+        if (err) {
+            console.log(err);
+        }
+        res.render('index', {docs: docs});
+    });
+});
+
+
+app.listen(3000,"localhost",err => {
+    if (err) {
+        console.log("Something is wrong!!!");
+    }
+    else {
+        console.log("This server is listening!!");
+    }
+});
+
 
 console.log('Hello world');
